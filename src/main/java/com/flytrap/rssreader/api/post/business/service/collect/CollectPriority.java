26dee76effd8faf74fa20375debff22bd24a514f
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-03-30 22:53:27 +0900

    feat: 게시글 수집을 위한 노드 기반의 블로킹 큐 추가

---
+++ src/main/java/com/flytrap/rssreader/api/post/business/service/collect/CollectPriority.java
@@ -0,0 +1,5 @@
+package com.flytrap.rssreader.api.post.business.service.collect;
+
+public enum CollectPriority {
+    HIGH, LOW
+}
---
+++ src/main/java/com/flytrap/rssreader/api/post/business/service/collect/SubscribeCollectionPriorityBlockingQueue.java
@@ -0,0 +1,43 @@
+package com.flytrap.rssreader.api.post.business.service.collect;
+
+import com.flytrap.rssreader.api.subscribe.infrastructure.entity.SubscribeEntity;
+import java.util.Deque;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SubscribeCollectionPriorityBlockingQueue implements SubscribeCollectionPriorityQueue {
+
+    private final Deque<SubscribeEntity> queue = new ConcurrentLinkedDeque<>();
+
+    @Override
+    public void add(SubscribeEntity subscribe, CollectPriority priority) {
+        if (priority == CollectPriority.HIGH) {
+            queue.addFirst(subscribe);
+        } else {
+            queue.addLast(subscribe);
+        }
+    }
+
+    @Override
+    public void addAll(List<SubscribeEntity> subscribes, CollectPriority priority) {
+        if (priority == CollectPriority.HIGH) {
+            for (int i = subscribes.size() - 1; i >= 0; i--) {
+                queue.addFirst(subscribes.get(i));
+            }
+        } else {
+            queue.addAll(subscribes);
+        }
+    }
+
+    @Override
+    public SubscribeEntity poll() {
+        return queue.poll();
+    }
+
+    @Override
+    public boolean isQueueEmpty() {
+        return queue.isEmpty();
+    }
+}
