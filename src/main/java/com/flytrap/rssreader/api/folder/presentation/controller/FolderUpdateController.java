3efadc471500dfd24c5262e21282af82631b3719
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-03-30 22:58:19 +0900

    feat: PostCollectService 파싱과 저장 을 한 로직에 수행하도록 다시 개발

--- src/main/java/com/flytrap/rssreader/api/folder/presentation/controller/FolderUpdateController.java
+++ src/main/java/com/flytrap/rssreader/api/folder/presentation/controller/FolderUpdateController.java
@@ -1,6 +1,6 @@
 package com.flytrap.rssreader.api.folder.presentation.controller;
 
-import com.flytrap.rssreader.api.alert.business.service.AlertService;
+import com.flytrap.rssreader.api.auth.presentation.dto.SessionMember;
 import com.flytrap.rssreader.api.folder.business.service.FolderSubscribeService;
 import com.flytrap.rssreader.api.folder.business.service.FolderUpdateService;
 import com.flytrap.rssreader.api.folder.business.service.FolderVerifyService;
@@ -8,18 +8,24 @@ import com.flytrap.rssreader.api.folder.domain.Folder;
 import com.flytrap.rssreader.api.folder.domain.FolderSubscribe;
 import com.flytrap.rssreader.api.folder.presentation.controller.swagger.FolderUpdateControllerApi;
 import com.flytrap.rssreader.api.folder.presentation.dto.FolderRequest;
-import com.flytrap.rssreader.api.post.business.service.PostCollectService;
-import com.flytrap.rssreader.api.subscribe.domain.Subscribe;
-import com.flytrap.rssreader.global.model.ApplicationResponse;
-import com.flytrap.rssreader.api.auth.presentation.dto.SessionMember;
-import com.flytrap.rssreader.api.subscribe.presentation.dto.SubscribeRequest;
 import com.flytrap.rssreader.api.post.business.facade.OpenCheckFacade;
-import com.flytrap.rssreader.global.presentation.resolver.Login;
+import com.flytrap.rssreader.api.post.business.service.collect.PostCollectService;
 import com.flytrap.rssreader.api.subscribe.business.service.SubscribeService;
+import com.flytrap.rssreader.api.subscribe.domain.Subscribe;
+import com.flytrap.rssreader.api.subscribe.presentation.dto.SubscribeRequest;
+import com.flytrap.rssreader.global.model.ApplicationResponse;
+import com.flytrap.rssreader.global.presentation.resolver.Login;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,9 +36,8 @@ public class FolderUpdateController implements FolderUpdateControllerApi {
     private final FolderVerifyService folderVerifyService;
     private final SubscribeService subscribeService;
     private final FolderSubscribeService folderSubscribeService;
-    private final PostCollectService postCollectService;
-    private final AlertService alertService;
     private final OpenCheckFacade openCheckFacade;
+    private final PostCollectService postCollectService;
 
     @PostMapping
     public ApplicationResponse<FolderRequest.Response> createFolder(
@@ -75,16 +80,13 @@ public class FolderUpdateController implements FolderUpdateControllerApi {
             @PathVariable Long folderId,
             @Valid @RequestBody SubscribeRequest.CreateRequest request,
             @Login SessionMember member) {
-        //TODO: 존재하는 폴더인지 검증하는 로직 , 공유폴더초대받은 인지 검증
-        //TODO: 내가만든 폴더랑, 초대받은 그룹의 폴더인지 구분해서 구독해야합니다.
-        //지금 부분은 검증된 개인 폴더입니다.
+
         Folder verifiedFolder = folderVerifyService.getVerifiedAccessableFolder(folderId, member.id());
         Subscribe subscribe = subscribeService.subscribe(request);
-        folderSubscribeService.folderSubscribe(subscribe,
-                verifiedFolder.getId());
+        folderSubscribeService.folderSubscribe(subscribe, verifiedFolder.getId());
 
         if (subscribe.isNewSubscribe()) {
-            postCollectService.collectPostsFromNewSubscribe(subscribe);
+            postCollectService.addNewSubscribeForCollect(subscribe);
         }
 
         FolderSubscribe folderSubscribe = FolderSubscribe.from(subscribe);
