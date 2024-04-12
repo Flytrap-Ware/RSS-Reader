b8de74663ee899a36470bc9d137258c176615fce
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-04-13 06:27:52 +0900

    remove: 실수로 지우지 못한 객체들 다시 제거

--- src/main/java/com/flytrap/rssreader/api/alert/business/event/subscribe/AlertEvent.java
+++
@@ -1,7 +0,0 @@
-//package com.flytrap.rssreader.api.alert.business.event.subscribe;
-//
-//import com.flytrap.rssreader.global.event.EventHolder;
-//import com.flytrap.rssreader.api.alert.business.service.dto.AlertParam;
-//import lombok.Getter;
-//
-//public record AlertEventXXX(@Getter AlertParam value) implements EventHolder<AlertParam> {}
--- src/main/java/com/flytrap/rssreader/api/alert/business/event/subscribe/SubscribeEvent.java
+++
@@ -1,5 +0,0 @@
-//package com.flytrap.rssreader.api.alert.business.event.subscribe;
-//
-//import java.util.Map;
-//
-//public record SubscribeEvent(Long subscribeId, Map<String, String> posts) {}
