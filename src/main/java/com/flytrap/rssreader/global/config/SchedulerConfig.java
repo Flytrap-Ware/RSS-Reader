65c50be805986969d362e7dae1945d2df155739d
gamja <leekhy02@gmail.com>
2024-05-06 16:26:15 +0900

    feat: jobLancher를 관리할 스케줄러 설정 추가 (#166)

--- src/main/java/com/flytrap/rssreader/global/config/SchedulerConfig.java
+++ src/main/java/com/flytrap/rssreader/global/config/SchedulerConfig.java
@@ -1,18 +1,38 @@
 package com.flytrap.rssreader.global.config;
 
-import org.springframework.context.annotation.Bean;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
+@EnableScheduling
+@RequiredArgsConstructor
+@Slf4j
 public class SchedulerConfig {
 
-    private static final int THREADS_COUNT = 2;
+    private final Job job;
+    private final JobLauncher jobLauncher;
 
-    @Bean
-    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
-        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
-        threadPoolTaskScheduler.setPoolSize(THREADS_COUNT);
-        return threadPoolTaskScheduler;
+    // @Scheduled(cron = "${scheduler.cron-job}")
+    @Scheduled(fixedDelay = 1000)
+    public void runJob() {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("JobID", String.valueOf(System.currentTimeMillis()))
+                .toJobParameters();
+
+        try {
+            log.debug("Job in execution: {}", job.getName());
+            JobExecution jobExecution = jobLauncher.run(job, jobParameters);
+            log.debug("Job completed with state: {}", jobExecution.getStatus());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
