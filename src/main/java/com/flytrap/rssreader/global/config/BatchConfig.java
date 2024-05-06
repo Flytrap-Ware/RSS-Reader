b30491f87a77956836d99b1fdeba77f0c3394836
gamja <leekhy02@gmail.com>
2024-05-06 16:26:53 +0900

    feat: batch작업중 job을 관리할 설정 클래스 추가 (#166)

---
+++ src/main/java/com/flytrap/rssreader/global/config/BatchConfig.java
@@ -0,0 +1,26 @@
+package com.flytrap.rssreader.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class BatchConfig {
+
+    private final JobRepository jobRepository;
+
+    @Bean
+    public Job crawlingJob(Step crawlingStep) {
+        return new JobBuilder("crawlingJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .flow(crawlingStep)
+                .end()
+                .build();
+    }
+}
