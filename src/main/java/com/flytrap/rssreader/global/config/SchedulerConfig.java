7a39c49d96c1c253ed9621f5650a6d6177b9d180
gamja <leekhy02@gmail.com>
2024-05-12 19:39:32 +0900

    feat: 크롤링,통계 스케줄러 설정 (#166)

--- src/main/java/com/flytrap/rssreader/global/config/SchedulerConfig.java
+++ src/main/java/com/flytrap/rssreader/global/config/SchedulerConfig.java
@@ -11,18 +11,37 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Configuration
 @EnableScheduling
 @RequiredArgsConstructor
 @Slf4j
 public class SchedulerConfig {
 
-    private final Job job;
+    private final Job crawlingJob;
+    private final Job statJob; // 통계 Job 추가
     private final JobLauncher jobLauncher;
 
-    // @Scheduled(cron = "${scheduler.cron-job}")
+    //크롤링 Job 실행 스케줄러
     @Scheduled(fixedDelay = 1000)
     public void runJob() {
+        executeJob(crawlingJob);
+    }
+
+    // 통계 Job 실행 스케줄러
+    @Scheduled(cron = "0 0 23 * * *") // 매일 오후 11시에 실행
+    public void runStatisticsJob() {
+        executeJob(statJob);
+    }
+
+    private void executeJob(Job job) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = now.format(formatter);
+        log.debug("작업 시작 시간: {}", formattedDateTime);
+
         JobParameters jobParameters = new JobParametersBuilder()
                 .addString("JobID", String.valueOf(System.currentTimeMillis()))
                 .toJobParameters();
