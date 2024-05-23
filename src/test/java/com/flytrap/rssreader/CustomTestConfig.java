a378a8efdd53e0be711c79319fa27ff60fb23b11
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-05-24 08:06:06 +0900

    chore: test container 라이브러리 추가 및 테스트 설정

---
+++ src/test/java/com/flytrap/rssreader/CustomTestConfig.java
@@ -0,0 +1,19 @@
+package com.flytrap.rssreader;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest
+@Testcontainers
+@Sql(scripts = "/reset.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+public @interface CustomTestConfig {
+
+}
