3f101c5c27d13f6eddd66e687f9f4a7049621d00
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-06-16 14:25:46 +0900

    fix: 수집한 게시글 없을 경우 방생하는 MyBAtis SQL문 에러 수정
    게시글이 없을 때 SQL문이 완성이 안됐음. 게시글이 첪는 경우를 처리함

--- src/main/java/com/flytrap/rssreader/api/post/infrastructure/repository/PostMyBatisRepository.java
+++ src/main/java/com/flytrap/rssreader/api/post/infrastructure/repository/PostMyBatisRepository.java
@@ -2,10 +2,28 @@ package com.flytrap.rssreader.api.post.infrastructure.repository;
 
 import com.flytrap.rssreader.api.post.infrastructure.entity.PostEntity;
 import java.util.List;
-import org.apache.ibatis.annotations.Mapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
-@Mapper
-public interface PostMyBatisRepository {
+@Repository
+@RequiredArgsConstructor
+public class PostMyBatisRepository {
 
-    int bulkUpsert(List<PostEntity> postEntities);
+    private final PostMyBatisMapper postMyBatisMapper;
+
+    /**
+     * PostEntity를 Bulk Upsert합니다.
+     *
+     * PostEntity 리스트가 null이 아니거나 비어 있지 않은지 확인합니다.
+     * 리스트가 null이거나 비어 있으면 -1을 반환하여 오류 또는 작업이 수행되지 않았음을 나타냅니다.
+     *
+     * @param postEntities Bulk Upsert할 PostEntity 리스트
+     * @return 리스트가 null이거나 비어 있는 경우, 그외 에러가 나면 -1을 반환하고, 그렇지 않으면 bulk upsert된 Entity의 수를 결과를 반환합니다.
+     */
+    public int bulkUpsert(List<PostEntity> postEntities) {
+        if (postEntities == null || postEntities.isEmpty()) {
+            return -1;
+        }
+        return postMyBatisMapper.bulkUpsert(postEntities);
+    }
 }
